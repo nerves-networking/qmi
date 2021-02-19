f8772400e29fca22a7344174c649ef3584cb7030
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-02-19 13:41:31 -0700

    Support getting operating state mode

--- lib/qmi.ex
+++ lib/qmi.ex
@@ -3,6 +3,35 @@ defmodule QMI do
   @type service :: non_neg_integer()
   @type control_point :: {service(), client_id()}
 
+  alias QMI.Service.DeviceManagement
+
   defdelegate get_control_point(driver, service), to: QMI.Service.Control
   defdelegate release_control_point(driver, cp), to: QMI.Service.Control
+
+  @doc """
+  Get the operating state of the modem
+  """
+  def get_operating_state() do
+    driver = start_driver()
+    cp = get_control_point(driver, DeviceManagement.id())
+
+    response = DeviceManagement.get_operating_state_mode(drive, cp)
+
+    release_control_point(driver, cp)
+
+    response
+  end
+
+  defp start_driver() do
+    # Will change in the future to not be so drastic, just
+    # was wanting to make working with this at the command line
+    # a little nicer for testing.
+    case QMI.Driver.start_link("/dev/cdc-wdm0") do
+      {:ok, driver} ->
+        driver
+
+      {:error, {:already_started, driver}} ->
+        driver
+    end
+  end
 end
---
+++ lib/qmi/services/device_manangement.ex
@@ -0,0 +1,46 @@
+defmodule QMI.Service.DeviceManagement do
+  use QMI.Service, id: 0x02
+
+  @get_operating_state_mode 0x002D
+
+  @doc """
+  Get the operating state mode
+
+  The operating state mode should be `:online` before trying to make a wireless
+  data network connection.
+  """
+  def get_operating_state_mode(driver, control_point) do
+    # Had to pad 3 bytes
+    # output from `strace -x -o /data/qmicli-trace qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode`
+    # write(5, "\x01\x0c\x00\x00\x02\x01\x00\x01\x00\x2d\x00\x00\x00", 13) = 13
+    # `\x2d` is the start of our request, and it shows that
+    bin = <<@get_operating_state_mode::16-little, 0x00, 0x00, 0x00>>
+
+    QMI.Driver.request(driver, bin, control_point)
+  end
+
+  @impl QMI.Service
+  def decode_response_tlvs(%{code: code} = response) when code not in [0, :success] do
+    response
+  end
+
+  def decode_response_tlvs(%{id: @get_operating_state_mode} = response) do
+    mode = decode_mode_tlvs(response.tlvs)
+
+    %{response | tlvs: [mode: mode]}
+  end
+
+  defp decode_mode_tlvs(<<0x01, 0x01, mode, _rest::binary>>) do
+    mode_from_byte(mode)
+  end
+
+  defp mode_from_byte(0x00), do: :online
+  defp mode_from_byte(0x01), do: :low_power
+  defp mode_from_byte(0x02), do: :factory_test
+  defp mode_from_byte(0x03), do: :offline
+  defp mode_from_byte(0x04), do: :resetting
+  defp mode_from_byte(0x05), do: :shutting_down
+  defp mode_from_byte(0x06), do: :persistent_low_power
+  defp mode_from_byte(0x07), do: :mode_only_low_power
+  defp mode_from_byte(0x08), do: :net_test_gw
+end
