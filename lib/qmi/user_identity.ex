38762dc02c44841253736235944ee61fb2844ff0
Matt Ludwigs <mattludwigs@gmail.com>
2021-05-21 08:30:37 -0700

    Add support for parsing ICCID raw binaries

--- lib/qmi/user_identity.ex
+++ lib/qmi/user_identity.ex
@@ -14,4 +14,25 @@ defmodule QMI.UserIdentity do
     Codec.UserIdentity.read_transparent(file_id, file_path)
     |> QMI.call(qmi)
   end
+
+  @doc """
+  Parse a raw binary ICCID
+
+  Call `read_transparent/3` to get the raw binary CCID.
+
+  ### Examples
+
+     iex> raw_binary = <<0x64, 0x73, 0x3, 0x4, 0x0, 0x0, 0x10, 0x52, 0x70, 0x20>>
+     iex> QMI.UserIdentity.parse_iccid(raw_binary)
+     "46373040000001250702"
+  """
+  @spec parse_iccid(binary()) :: binary()
+  def parse_iccid(binary) do
+    for <<a::4, b::4 <- binary>>, into: "" do
+      int_to_string(b * 10 + a)
+    end
+  end
+
+  defp int_to_string(x) when x < 10, do: "0" <> Integer.to_string(x)
+  defp int_to_string(x), do: Integer.to_string(x)
 end
