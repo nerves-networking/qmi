fca41dbd10e3bc88b2af58f7372147514885c058
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-08-20 09:39:56 -0700

    Parse packet service status indication

--- lib/qmi/codec/wireless_data.ex
+++ lib/qmi/codec/wireless_data.ex
@@ -4,6 +4,7 @@ defmodule QMI.Codec.WirelessData do
   """
 
   @start_network_interface 0x0020
+  @packet_service_status_ind 0x0022
 
   @typedoc """
   Options for whens starting a network interface
@@ -16,6 +17,40 @@ defmodule QMI.Codec.WirelessData do
   Report from starting the network interface
   """
   @type start_network_report() :: %{packet_data_handle: non_neg_integer()}
+
+  @typedoc """
+  The type of reasons the call ended
+  """
+  @type call_end_reason_type() ::
+          :unspecified
+          | :mobile_ip
+          | :internal
+          | :call_manger_defined
+          | :three_gpp_specification_defined
+          | :ppp
+          | :ehrpd
+          | :ipv6
+          | :handoff
+
+  @typedoc """
+  Name of the technology
+  """
+  @type tech_name() ::
+          :cdma | :umts | :wlan_local_brkout | :iwlan_s2b | :epc | :embms | :modem_link_local
+
+  @typedoc """
+  The indication for a change in the current packet data connection status
+  """
+  @type packet_status_indication() :: %{
+          status: :disconnected | :connected | :suspended | :authenticating,
+          reconfiguration_required: boolean(),
+          call_end_reason: integer() | nil,
+          call_end_reason_type: call_end_reason_type() | nil,
+          ip_family: 4 | 6 | nil,
+          tech_name: tech_name() | nil,
+          bearer_id: integer() | nil,
+          xlat_capable: boolean() | nil
+        }
 
   @doc """
   Send command to request the that the network interface starts to receive data
@@ -68,4 +103,128 @@ defmodule QMI.Codec.WirelessData do
        ) do
     parse_start_network_interface_tlvs(parsed, rest)
   end
+
+  @doc """
+  Try to parse an indication from the wireless data service
+  """
+  @spec parse_indication(binary()) ::
+          {:ok, packet_status_indication()} | {:error, :invalid_indication}
+  def parse_indication(
+        <<@packet_service_status_ind::16-little, size::16-little, tlvs::binary-size(size)>>
+      ) do
+    result = parse_packet_status_indication(packet_status_indication_init(), tlvs)
+
+    {:ok, result}
+  end
+
+  def parse_indication(_other) do
+    {:error, :invalid_indication}
+  end
+
+  defp packet_status_indication_init() do
+    %{
+      status: nil,
+      reconfiguration_required: nil,
+      call_end_reason: nil,
+      call_end_reason_type: nil,
+      ip_family: nil,
+      tech_name: nil,
+      bearer_id: nil,
+      xlat_capable: nil
+    }
+  end
+
+  defp parse_packet_status_indication(indication, <<>>) do
+    indication
+  end
+
+  defp parse_packet_status_indication(
+         indication,
+         <<0x01, 0x02, 0x00, connection_status, reconfig_required, rest::binary>>
+       ) do
+    indication
+    |> Map.put(:status, parse_packet_status(connection_status))
+    |> Map.put(:reconfiguration_required, reconfig_required == 1)
+    |> parse_packet_status_indication(rest)
+  end
+
+  defp parse_packet_status_indication(
+         indication,
+         <<0x10, 0x02, 0x00, call_end_reason::16-little, rest::binary>>
+       ) do
+    indication
+    |> Map.put(:call_end_reason, call_end_reason)
+    |> parse_packet_status_indication(rest)
+  end
+
+  defp parse_packet_status_indication(
+         indication,
+         <<0x11, 0x04, 0x00, call_end_reason_type::16-little, call_end_reason::16-little,
+           rest::binary>>
+       ) do
+    indication
+    |> Map.put(:call_end_reason_type, parse_call_end_reason_type(call_end_reason_type))
+    |> Map.put(:call_end_reason, call_end_reason)
+    |> parse_packet_status_indication(rest)
+  end
+
+  defp parse_packet_status_indication(indication, <<0x12, 0x01, 0x00, ip_family, rest::binary>>) do
+    indication
+    |> Map.put(:ip_family, ip_family)
+    |> parse_packet_status_indication(rest)
+  end
+
+  defp parse_packet_status_indication(
+         indication,
+         <<0x13, 0x02, 0x00, tech_name::16-little, rest::binary>>
+       ) do
+    indication
+    |> Map.put(:tech_name, parse_tech_name(tech_name))
+    |> parse_packet_status_indication(rest)
+  end
+
+  defp parse_packet_status_indication(indication, <<0x14, 0x01, 0x00, bearer_id, rest::binary>>) do
+    indication
+    |> Map.put(:bearer_id, bearer_id)
+    |> parse_packet_status_indication(rest)
+  end
+
+  defp parse_packet_status_indication(
+         indication,
+         <<0x15, 0x01, 0x00, xlat_capable, rest::binary>>
+       ) do
+    indication
+    |> Map.put(:xlat_capable, xlat_capable == 1)
+    |> parse_packet_status_indication(rest)
+  end
+
+  defp parse_packet_status_indication(
+         indication,
+         <<_type, length::16-little, _values::binary-size(length), rest::binary>>
+       ) do
+    parse_packet_status_indication(indication, rest)
+  end
+
+  defp parse_packet_status(0x01), do: :disconnected
+  defp parse_packet_status(0x02), do: :connected
+  defp parse_packet_status(0x03), do: :suspended
+  defp parse_packet_status(0x04), do: :authenticating
+
+  defp parse_tech_name(0x8001), do: :cdma
+  defp parse_tech_name(0x8004), do: :umts
+  defp parse_tech_name(0x8020), do: :wlan_local_brkout
+  defp parse_tech_name(0x8021), do: :iwlan_s2b
+  defp parse_tech_name(0x8880), do: :epc
+  defp parse_tech_name(0x8882), do: :embms
+  defp parse_tech_name(0x8888), do: :modem_link_local
+
+  defp parse_call_end_reason_type(0x00), do: :unspecified
+  defp parse_call_end_reason_type(0x01), do: :mobile_ip
+  defp parse_call_end_reason_type(0x02), do: :internal
+  defp parse_call_end_reason_type(0x03), do: :call_manager_defined
+  defp parse_call_end_reason_type(0x06), do: :three_gpp_specification_defined
+  defp parse_call_end_reason_type(0x07), do: :ppp
+  defp parse_call_end_reason_type(0x08), do: :ehrpd
+  defp parse_call_end_reason_type(0x09), do: :ipv6
+  defp parse_call_end_reason_type(0x0C), do: :handoff
 end
--- test/qmi/codec/wireless_data_test.exs
+++ test/qmi/codec/wireless_data_test.exs
@@ -17,4 +17,182 @@ defmodule QMI.Codec.WirelessDataTest do
            ) ==
              {:ok, %{packet_data_handle: 0x01}}
   end
+
+  describe "parsing packet status indication" do
+    test "when connected" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x01, 0x02, 0x00, 0x02, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.status == :connected
+    end
+
+    test "when disconnected" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x01, 0x02, 0x00, 0x01, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.status == :disconnected
+    end
+
+    test "when suspended" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x01, 0x02, 0x00, 0x03, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.status == :suspended
+    end
+
+    test "when authenticating" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x01, 0x02, 0x00, 0x04, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.status == :authenticating
+    end
+
+    test "when reconfiguring required" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x01, 0x02, 0x00, 0x04, 0x01>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.reconfiguration_required == true
+    end
+
+    test "when reconfiguring not required" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x01, 0x02, 0x00, 0x04, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.reconfiguration_required == false
+    end
+
+    test "when call end reason type is unspecified" do
+      binary = <<0x22, 0x00, 0x07, 0x00, 0x11, 0x04, 0x00, 0x00::16-little, 0x00, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.call_end_reason_type == :unspecified
+    end
+
+    test "when call end reason type is mobile ip" do
+      binary = <<0x22, 0x00, 0x07, 0x00, 0x11, 0x04, 0x00, 0x01::16-little, 0x00, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.call_end_reason_type == :mobile_ip
+    end
+
+    test "when call end reason type is internal" do
+      binary = <<0x22, 0x00, 0x07, 0x00, 0x11, 0x04, 0x00, 0x02::16-little, 0x00, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.call_end_reason_type == :internal
+    end
+
+    test "when call end reason type is call manager defined" do
+      binary = <<0x22, 0x00, 0x07, 0x00, 0x11, 0x04, 0x00, 0x03::16-little, 0x00, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.call_end_reason_type == :call_manager_defined
+    end
+
+    test "when call end reason type is 3GPP defined" do
+      binary = <<0x22, 0x00, 0x07, 0x00, 0x11, 0x04, 0x00, 0x06::16-little, 0x00, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.call_end_reason_type == :three_gpp_specification_defined
+    end
+
+    test "when call end reason type is PPP" do
+      binary = <<0x22, 0x00, 0x07, 0x00, 0x11, 0x04, 0x00, 0x07::16-little, 0x00, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.call_end_reason_type == :ppp
+    end
+
+    test "when call end reason type is EHRPD" do
+      binary = <<0x22, 0x00, 0x07, 0x00, 0x11, 0x04, 0x00, 0x08::16-little, 0x00, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.call_end_reason_type == :ehrpd
+    end
+
+    test "when call end reason type is IPv6" do
+      binary = <<0x22, 0x00, 0x07, 0x00, 0x11, 0x04, 0x00, 0x09::16-little, 0x00, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.call_end_reason_type == :ipv6
+    end
+
+    test "when call end reason type is handoff" do
+      binary = <<0x22, 0x00, 0x07, 0x00, 0x11, 0x04, 0x00, 0x0C::16-little, 0x00, 0x00>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.call_end_reason_type == :handoff
+    end
+
+    test "when tech name is CDMA" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x13, 0x02, 0x00, 0x8001::16-little>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.tech_name == :cdma
+    end
+
+    test "when tech name is UMTS" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x13, 0x02, 0x00, 0x8004::16-little>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.tech_name == :umts
+    end
+
+    test "when tech name is WLAN Local brkout" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x13, 0x02, 0x00, 0x8020::16-little>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.tech_name == :wlan_local_brkout
+    end
+
+    test "when tech name is iWLAN S2b" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x13, 0x02, 0x00, 0x8021::16-little>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.tech_name == :iwlan_s2b
+    end
+
+    test "when tech name is EPC" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x13, 0x02, 0x00, 0x8880::16-little>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.tech_name == :epc
+    end
+
+    test "when tech name is EMBMS" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x13, 0x02, 0x00, 0x8882::16-little>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.tech_name == :embms
+    end
+
+    test "when tech name is modem link local" do
+      binary = <<0x22, 0x00, 0x05, 0x00, 0x13, 0x02, 0x00, 0x8888::16-little>>
+
+      {:ok, indication} = WirelessData.parse_indication(binary)
+
+      assert indication.tech_name == :modem_link_local
+    end
+  end
 end
