5eed4dc9af57347bd6e86f57d52a3342b97d767e
Jon Carstens <jjcarstens@me.com>
2021-08-20 08:09:20 -0600

    device manufacturer

--- test/qmi/codec/device_management_test.exs
+++ test/qmi/codec/device_management_test.exs
@@ -16,4 +16,17 @@ defmodule QMI.Codec.DeviceManagementTest do
                0x3A, 0x30, 0x30, 0x3A, 0x30, 0x30, 0x5D>>
            ) == {:ok, "25.20.266  1  [Jun 21 2019 13:00:00]"}
   end
+
+  test "get_device_mfr/0" do
+    request = DeviceManagement.get_device_mfr()
+
+    assert IO.iodata_to_binary(request.payload) == <<33, 0, 0, 0>>
+    assert request.service_id == 0x02
+
+    response_bin =
+      <<33, 0, 31, 0, 2, 4, 0, 0, 0, 0, 0, 1, 21, 0, 81, 85, 65, 76, 67, 79, 77, 77, 32, 73, 78,
+        67, 79, 82, 80, 79, 82, 65, 84, 69, 68>>
+
+    assert request.decode.(response_bin) == {:ok, "QUALCOMM INCORPORATED"}
+  end
 end
